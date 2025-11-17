82ab067b76e1332295a693a6e8a66668cf11bf51
Leo <leonid112@yandex.ru>
2025-11-17 13:39:29 +0300

    new project

--- main.py
+++ main.py
@@ -1,88 +1,42 @@
-#список покупок с БД
 #шаблон https://www.pythontutorial.net
 #https://github.com/leonid1123/pk-211-start
+# повторить подключение и работу с БД
 import sys
 from pathlib import Path
-import sqlite3
-from PyQt6.QtCore import Qt, QSize
-from PyQt6.QtWidgets import QApplication, QWidget, \
-    QGridLayout, QLineEdit, QSpinBox, QSlider, QLabel, QPushButton, \
-    QListWidget
-
+from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QListWidget,\
+QPushButton, QLineEdit, QLabel
+import pymysql.cursors
 
 class MainWindow(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.conn = sqlite3.connect("pokupki.db")
-        self.cur = self.conn.cursor()
-        self.cur.execute(
-            "CREATE TABLE IF NOT EXISTS pokupki (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price INTEGER, kolichestvo INTEGER)")
-        self.cur.execute("SELECT name,price,kolichestvo FROM pokupki")
-        ans = self.cur.fetchall()
-
-        self.sum = 0
-        self.setWindowTitle('Список покупок')
+        self.setWindowTitle('КОЛБАСА!!!!')
+        self.showMaximized()
         layout = QGridLayout()
         self.setLayout(layout)
-        self.name_entry = QLineEdit(maxLength=100,
-                                    placeholderText="Название"
-                                    )
-        layout.addWidget(self.name_entry, 0, 0)
-        self.quantity_entry = QSpinBox(
-            minimum=1,
-            maximum=1000,
-            singleStep=10,
-            suffix="руб.",
-            value=100
-        )
-        layout.addWidget(self.quantity_entry, 1, 0)
-        self.slider = QSlider(Qt.Orientation.Horizontal)
-        self.slider.valueChanged.connect(self.show_value)
-        self.slider.setTickPosition(
-            QSlider.TickPosition.TicksBelow
-        )
-        self.slider.setRange(1, 10)
-        self.slider.setSingleStep(1)
-        self.slider.setPageStep(1)
-        layout.addWidget(self.slider, 2, 0)
-        self.value_label = QLabel("Количество:")
-        layout.addWidget(self.value_label, 3, 0)
-        self.add_btn = QPushButton("+")
-        self.add_btn.clicked.connect(self.add_tovar)
-        layout.addWidget(self.add_btn, 0, 1, 3, 1)
-        self.add_btn.setFixedSize(QSize(100, 150))
-        self.goods_lst = QListWidget()
-        for item in ans:
-            self.goods_lst.addItem(f"{item[0]} {item[1]}руб. {item[2]}")
-        layout.addWidget(self.goods_lst,3,0,1,2)
-        self.total = QLabel("Итого:")
-        layout.addWidget(self.total,4,0)
+
+        self.kolbasa_view = QListWidget()
+        self.show_all_btn = QPushButton("Показать")
+        self.show_all_btn.clicked.connect(self.get_all)
+        self.clear_all_btn = QPushButton("Очистить список")
+
+        layout.addWidget(QLabel('Список колбасы'),0,0,1,2)
+        layout.addWidget(self.kolbasa_view,1,0,1,2)
+        layout.addWidget(self.show_all_btn,2,0)
+        layout.addWidget(self.clear_all_btn,2,1)
         self.show()
 
-    def show_value(self, value):
-        print(value)
-        #сюда написать значение от ползунка!
-
-    def add_tovar(self):
-        name = self.name_entry.text()
-        value = self.quantity_entry.value()
-        kol = self.slider.value()
-        """self.goods_lst.addItem(f"Название:{name}, цена: {value}, количество: {kol}")
-        x = value * kol
-        self.sum = self.sum + x"""
-        self.total.setText(f"ИТОГО: {self.sum}")
-        sql = 'INSERT INTO pokupki(name, price, kolichestvo) VALUES(?,?,?)'
-        self.cur.execute(sql,(name, value, kol))
-        self.conn.commit()
-        sql = 'SELECT name,price,kolichestvo FROM pokupki'
-        self.cur.execute(sql)
-        ans = self.cur.fetchall()
-        self.goods_lst.clear()
-        for item in ans:
-            self.goods_lst.addItem(f"Название:{item[0]}, цена:{item[1]}, кол-во:{item[2]}")
-
-
-
+    def get_all(self):
+        self.conn = pymysql.connect(
+            host='localhost',
+            user='pk211',
+            password='1234',
+            database='pk211_kolbasa'
+        )
+        self.cur = self.conn.cursor()
+        self.cur.execute('SELECT * FROM kolbasa')
+        ans = self.cur.fetchone()
+        print(ans)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
